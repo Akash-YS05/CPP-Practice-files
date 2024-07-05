7771460d4919474ca535e2cc43d6059686a8bf8f
Akash-YS05 <akash.pan05@gmail.com>
2024-07-05 17:57:20 +0530

    do while and for loops

---
+++ for_loops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 4; i++) {
+        std::cout << "Number rn is " << i << '\n';
+    }
+
+    for (int i=10;i>=1;i--) {
+        std::cout << i <<". . .\n";
+    }
+
+    std::cout << "Happy New Year!!!!\n";
+}
